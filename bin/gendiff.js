8d1d6a8de8bbce3d602e4dd22bed019cbabf4a2b
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2021-05-19 15:28:58 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,11 @@ const program = new Command();
 program
   .version('0.0.1', '-v, --version', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
+
 
 
 
