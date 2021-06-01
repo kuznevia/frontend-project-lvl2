6772bc322a182beb4c396c2276eef8f3cec57fe8
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2021-06-01 15:09:42 +0300

    tests not working

---
+++ __tests__/index.test.js
@@ -0,0 +1,20 @@
+import * as path from 'path';
+import { readFileSync } from 'fs';
+import genDiff from '../src/index.js';
+
+const answerPath = path.resolve(process.cwd(), './__tests__/__fixtures__/correct.txt');
+const answer = readFileSync(answerPath, 'utf8');
+console.log(answer);
+
+test('genDiff JSON', () => {
+  expect(genDiff('file1.json', 'file2.json')).toEqual(answer);
+});
+
+test('genDiff YML', () => {
+  expect(genDiff('file1.yml', 'file2.yml')).toEqual(answer);
+});
+
+test('genDiff JSON/YML', () => {
+  expect(genDiff('file1.json', 'file2.yml')).toEqual(answer);
+  expect(genDiff('file1.yml', 'file2.json')).toEqual(answer);
+});
--- src/parsers.js
+++ src/parsers.js
@@ -8,7 +8,7 @@ const parsersTree = {
 };
 
 export default (file) => {
-  const filePath = path.resolve(process.cwd(file), file);
+  const filePath = path.resolve(process.cwd(), './__tests__/__fixtures__', file);
   const fileExt = path.extname(filePath);
   const fileContent = readFileSync(filePath, 'utf8');
   const parse = parsersTree[fileExt];
